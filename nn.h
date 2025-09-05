8760430a2e7c6daa76fe3d59160df357244ff592
nalesiddharth <nale.siddharth24@vit.edu>
2025-09-05 19:41:32 +0530

    nil

---
+++ nn.h
@@ -0,0 +1,19 @@
+#ifndef NN_H
+#define NN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#endif // NN_H
+
+
+
+
+
+#ifdef NN_IMPLEMENTATION
+
+
+
+#endif // NN_IMPLEMENTATION
